785180ce5eeca90081e1e10081960d826bcbdaab
zyar3k <a.zygan@gmail.com>
2021-10-25 13:27:01 +0200

    Connect to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DBConnect success"))
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(5000, () => {
   console.log("Backend server is running!");
