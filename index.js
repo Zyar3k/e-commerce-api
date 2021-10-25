90842e8e5bdf959b58a3f4a8c09970618508e58d
zyar3k <a.zygan@gmail.com>
2021-10-25 13:41:28 +0200

    Testing routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRoute = require("./routes/user");
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -13,6 +14,9 @@ mongoose
     console.log(error);
   });
 
-app.listen(5000, () => {
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
