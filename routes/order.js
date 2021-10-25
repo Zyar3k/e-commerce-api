99607cefebe1a4d3ea586def4c4642f915367b12
zyar3k <a.zygan@gmail.com>
2021-10-25 21:26:58 +0200

    Add routes for monthly income

--- routes/order.js
+++ routes/order.js
@@ -13,7 +13,7 @@ router.post("/", verifyToken, async (req, res) => {
   const newOrder = new Order(req.body);
   try {
     const savedOrder = await newOrder.save();
-    res.status(200).json(savedCart);
+    res.status(200).json(savedOrder);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -64,6 +64,34 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
     res.status(200).json(orders);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+// get monthly income
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
